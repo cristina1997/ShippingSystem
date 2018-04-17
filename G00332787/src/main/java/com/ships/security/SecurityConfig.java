88f31360181d64fe37c3e57d9ebc0ae4265240cc
cristina1997 <cristinaflorinaeu@gmail.com>
2018-04-17 16:29:08 +0100

    Security Done

---
+++ G00332787/src/main/java/com/ships/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.ships.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{ 
+  @Override
+  public void configure(HttpSecurity httpSecurity) throws Exception{
+    httpSecurity.authorizeRequests()
+     .antMatchers("/addShip", "/addShippingCompany", "/createOrder")
+     .hasRole("USER")
+     .and()
+     .formLogin();
+  }
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) 
+						throws Exception {
+    auth.inMemoryAuthentication()
+      .withUser("user").password("user").roles("USER");
+  }
+}
